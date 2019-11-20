5a7e8287a80176eef9cefe0c7badfbcffcaffac8
leonardodalcin <leon.dalcin@gmail.com>
2019-11-20 18:29:06 -0300

    improvements

--- source/outputPlugins/convertEntitiesToDotLanguage.ts
+++ source/outputPlugins/convertEntitiesToDotLanguage.ts
@@ -1,18 +1,30 @@
 import { IEntity } from '@interfaces/IEntity'
-import { IPropertyType } from '@interfaces/IPropertyType'
 import { digraph } from 'graphviz'
 import { resolve } from 'path'
 
+function entityToNodeLabel(entity: IEntity) {
+  const propLabels = entity.properties.map((property) => {
+    return `${property.name} : ${property.types[0]}`
+  })
+  const label = `{${entity.name} | ${propLabels.reduce(
+    (previousValue = '', currentValue) => {
+      return `${previousValue}\\l${currentValue}`
+    }
+  )}}`
+  return label
+}
 export function convertEntitiesToDotLanguage(entities: IEntity[]) {
   const graph = digraph('1')
   for (const entity of entities) {
-    const mapped: { [propName: string]: IPropertyType } = {}
-    for (const property of entity.properties) {
-      mapped[property.name] = property.types[0]
-    }
-    graph.addNode(entity.name, { label: JSON.stringify(mapped, undefined, 2).replace(/"/g, '') })
-
+    graph.addNode(entity.name, {
+      shape: 'record',
+      label: entityToNodeLabel(entity)
+    })
   }
   graph.output('png', resolve(__dirname + '.png'))
+  graph.output('dot', resolve(__dirname + '.dot'))
+  graph.output('svg', resolve(__dirname + '.svg'))
+  graph.output('xml', resolve(__dirname + '.xml'))
+
   return graph.to_dot()
 }
